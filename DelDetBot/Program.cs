e16ebd9c3d0e9a9c5078cce67770b1390b758a59
Kresten Laust <krestenlaust@gmail.com>
2023-06-04 22:49:48 +0200

    Merged the BotInstance project with the DelDetBot project

--- DelDetBot/Program.cs
+++ DelDetBot/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using DelDetBot;
 
-namespace BotInstance
+namespace DelDetBot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DelDenBot bot = new DelDenBot("10.242.79.240", 25565);
+            DelDenBot bot = new DelDenBot("192.168.1.10", 25565);
 
             var task = Task.Run(async () => await bot.RunBotAsync());
 
