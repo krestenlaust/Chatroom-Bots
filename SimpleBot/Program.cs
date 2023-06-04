a753ce7160c1a2f69e173898a59078b00cd4e1c8
Kresten Laust <krestenlaust@gmail.com>
2023-06-04 22:44:49 +0200

    Added SimpleBot from Chatroom-NetworkClient repository

---
+++ SimpleBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using Chatroom.NetworkClient.Bot;
+
+namespace SimpleBot
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Bot testBot = new SimpleBot("192.168.1.10", 25565);
+
+            while (true)
+            {
+                testBot.Update();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ SimpleBot/SimpleBot.cs
@@ -0,0 +1,34 @@
+﻿using Chatroom.NetworkClient.Bot;
+
+namespace SimpleBot
+{
+    /// <summary>
+    /// Sample implementation of a simple bot.
+    /// </summary>
+    public class SimpleBot : Bot
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleBot"/> class.
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        public SimpleBot(string hostname, ushort port)
+            : base("Simpletron", hostname, port)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void OnMessage(Message message)
+        {
+            if (message.Author.IsSelf)
+            {
+                return;
+            }
+
+            if (message.Content.StartsWith("deldet"))
+            {
+                Send("let Johnny = del det!");
+            }
+        }
+    }
+}
